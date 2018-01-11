a5940c78f49d535dc54ed710b767a4a19b5a7eab
d4v1d41 <davepenott@protonmail.com>
2018-01-10 21:20:41 -0300

    Comments

--- src/Weather.java
+++ src/Weather.java
@@ -2,16 +2,28 @@ import com.jaunt.*;
 import com.jaunt.component.*;
 import java.util.List;
 import java.text.NumberFormat;
+/**
+*
+* @author D4v1d41
+*/
 public class Weather{
   public static void main(String[] args){
     try{
+    	// User Agent
       UserAgent userAgent = new UserAgent(); 
+      // Visit accuweather
       userAgent.visit("https://www.accuweather.com");
+      // find the first element with the given html
       Element city = userAgent.doc.findFirst("<div class=\"loc\">");
+      // print inner text of the city element, which is the text where displays the city
       System.out.println(city.innerText());
+      // Find first element w/ the given html 
       Element temp = userAgent.doc.findFirst("<span class=\"large-temp\">");
+      // declare string tvalue (temperature value)
       String tvalue = temp.innerText().replaceAll("&deg;", "");
+      // format the tvalue string to double, and already convert fahrenheit to celsius
       double num = ((Double.parseDouble(tvalue) - 32.0) * (5.0 / 9.0));
+      // print the temp in f and num, which is a double, with only two decimals
       System.out.printf("The actual temp is "+tvalue+" Fahrenheit."+" In Celsius: %.1f", num);
     }
     catch(JauntException e){                          
